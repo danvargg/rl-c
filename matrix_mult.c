ae614402e9ca598aab8a70e4ed5b32a50f26f5fe
Daniel Vargas <devg28@gmail.com>
2023-05-24 18:27:35 -0400

    basics done.

--- matrix_mult.c
+++ matrix_mult.c
@@ -1,8 +1,17 @@
-// Simple matrix multiplication
-#include <stdio.h>
+/*
+ * Performs matrix multiplication of two matrices.
+ *
+ * This function multiplies two matrices, `matrix_a` and `matrix_b`, and stores the result in the `result` matrix.
+ * The matrices are assumed to be stored in row-major order.
 
-// Function to perform matrix multiplication
-void matrix_multiply(double *matrix_a, double *matrix_b, double *result, int rows_a, int cols_a, int cols_b) {
+ * @param matrix_a Pointer to the first matrix.
+ * @param matrix_b Pointer to the second matrix.
+ * @param result Pointer to the result matrix.
+ * @param rows_a Number of rows in `matrix_a`.
+ * @param cols_a Number of columns in `matrix_a` and number of rows in `matrix_b`.
+ * @param cols_b Number of columns in `matrix_b`.
+ */
+void matrix_multiply(double const *matrix_a, double const *matrix_b, double *result, int rows_a, int cols_a, int cols_b) {
     for (int i = 0; i < rows_a; i++) {
         for (int j = 0; j < cols_b; j++) {
             double sum = 0.0;
--- one_o_one.c
+++ one_o_one.c
@@ -8,8 +8,13 @@ int main() {
 
     age = 30;
 
+    char password[] = "spatula";
+
     puts("Wait, what?");
     printf("More %d %d\n", 27, age + 1);
     printf("String: %c %s", 'C', "what");
+    printf("Char: %c %c %c\n", 'a', 'b', 'c');
+    printf("The password is %s\n", password);
+
     return 0;
 }
