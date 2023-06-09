21094463e9a9ca44cf9400d0b3a94579da62ebd9
Daniel Vargas <devg28@gmail.com>
2023-06-09 19:26:17 -0400

    more basics.

--- one_o_one.c
+++ one_o_one.c
@@ -1,18 +1,31 @@
 // First program
 
 #include <stdio.h>
+#include <ctype.h>
 
 int main() {
     int age = 30;
-    int a = 8;
+    int a = 0;
     char x, y, z;
     char password[] = "spatula";
     char k = 'b';
 
+    while (a < 20) {
+        printf("%d\n", a);
+        a++;
+    }
+
+    for (a = 0; a < 10; a++) {
+        printf("%d\n", a);
+        for (int b = 0; b < 10; b++) {
+            printf("%d\n", b);
+        }
+    }
+
     puts("Wait, what?");
     printf("More %d %d\n", 27, age + 1);
     printf("String: %c %s", 'C', "what");
-    printf("Char: %c %c %c\n", 'a', 'b', 'c');
+    printf("Char: %c %c %c\n", toupper('a'), 'b', 'c');
     printf("The password is %s\n", password);
 
     if (a >= 10) {
@@ -23,7 +36,7 @@ int main() {
         printf("Wrong!");
     }
 
-    switch(k) {
+    switch (k) {
         case 'a':
             printf("A\n");
             break;
@@ -34,5 +47,5 @@ int main() {
             printf("C\n");
     }
 
-    return (0);
+    return 0;
 }
